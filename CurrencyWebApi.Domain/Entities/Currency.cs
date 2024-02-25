eae6e8ebbb5b091597e5ff9ee53c86566c436039
Baturhan Aykut <baturhanaykut@gmail.com>
2024-02-25 13:47:00 +0300

    Cuurency and Currency detail entties were created

---
+++ CurrencyWebApi.Domain/Entities/Currency.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyWebApi.Domain.Entities
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? AtributeName { get; set; }
+    }
+}
---
+++ CurrencyWebApi.Domain/Entities/CurrenyDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyWebApi.Domain.Entities
+{
+    public class CurrenyDetail
+    {
+        public int CurrenyId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string? Value { get; set; }
+
+        public List<Currency>? Currencies { get; set; }
+    }
+}
