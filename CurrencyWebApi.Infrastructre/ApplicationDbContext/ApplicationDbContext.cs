937290848594cf712acb326e4c8c4c6d99f20247
Baturhan Aykut <baturhanaykut@gmail.com>
2024-02-25 14:07:59 +0300

    Infrastructure layer was created and Dbcontext class generated

---
+++ CurrencyWebApi.Infrastructre/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CurrencyWebApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyWebApi.Infrastructre.ApplicationDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<CurrenyDetail> CurrenyDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CurrencyConfig())
+                .ApplyConfiguration(new CurrencyDetailConfig());
+
+                base.OnModelCreating(modelBuilder);
+        }
+    }
+}
